281501a2e6bb04c26809304eef99a354dc3ca900
TomasOrava <129678116+TomasOrava@users.noreply.github.com>
2023-06-06 20:42:49 +0200

    Add files via upload

---
+++ Thread úkol/Thread test/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Text;
+
+namespace Thread_test
+{
+    public partial class Form1 : Form
+    {
+        clsTimer mobjTimer; // nový timer!!!!!
+
+
+        Thread mobjVlakno1;
+       // int mintCt;
+        object mobjSynchro;// nová proměnná --nejobecnější a nejednoduší´- nelze s ním nic dělat 
+        bool mblStop;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //start nového timeru 
+            mobjTimer = new clsTimer(1); // hazí hodnotu do classy
+            mobjTimer.Tick += clsTimer_tick; // += tam musí být - do zásobníku volání přídavám rutinu timer click,, adresa  
+            mobjTimer.mobjForm = this;  //že to je v tomtu formu
+            mobjTimer.Start();
+
+            //defionváaní nivého timeru v kódu 
+
+            
+        }
+
+        //tick timeru
+        void clsTimer_tick()
+        {
+            textBox1.Text = "ahoj";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mobjTimer.Stop();
+           
+        }
+    }
+}
---
+++ Thread úkol/Thread test/clsTimer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_test
+{
+    internal class clsTimer
+
+    {
+
+        //interval v milisekundach  , za úkol nastavení na mikro, máhodnotu 1 zadanou z formu řadek 35 button 1 click 
+        int mintIntervalIn_ms;
+
+        Thread mobjVlakno1; //thread pro časování timeru 
+  
+        bool mblStop;// zastavit thread 
+
+        // definice tavru eventu 
+        public delegate void dlgTick(); //deklarce ukazatele na funkci, delegat je ukazatel 
+        //deklarace eventu ticku 
+        public event dlgTick Tick;
+
+        //formulář kde existuje můj objekt 
+        public Form1 mobjForm;
+
+        public int Interval
+        {
+
+            get { return mintIntervalIn_ms; }   
+            set { mintIntervalIn_ms = value;}
+                
+
+        }
+
+        //-------------------------------------------------------------------------------------------------
+        // konstruktor timeru
+        //-------------------------------------------------------------------------------------------------
+        public clsTimer(int intIntervalIN_ms)
+        {
+            mintIntervalIn_ms = Math.Abs(intIntervalIN_ms);
+            mobjForm = null;
+        }
+
+
+        //-------------------------------------------------------------------------------------------------
+        // start timeru
+        //-------------------------------------------------------------------------------------------------
+
+
+       public bool Start()
+        {
+            // vytvoření threadu
+            mobjVlakno1 = new Thread(MojeRutina);
+
+            //spustit timer
+            mblStop = false;
+            
+            //mobjVlakno1.Priority = ThreadPriority.Highest;
+            mobjVlakno1.Start();
+
+
+            return true;
+        }
+
+        private void MojeRutina()
+        {
+            do
+            {
+                //zavolat eventy v threadu formuláře 
+                if (mobjForm != null) //=! - nerovná se 
+                mobjForm.Invoke(Tick);
+
+                //upravit tak aby thread běžěl v mikrosekundách - funkce DateTime.Ticks  - -něco vrací - smyčka která čte - tik za 1 až tisíc mikroseknud - timer využívá jedno jádro - pomocí ticks 
+
+
+                //pozastavit thread
+                //
+                //Thread.Sleep(mintIntervalIn_ms);
+                
+
+              //  do
+                //{
+                    // DateTime
+                    TimeSpan.FromMilliseconds(mintIntervalIn_ms / 1000); //převedení na mikrosekundy, v class nastavené timer = 1, (1 tick je o,1 mikrosekundy)
+              //  } while (mobjForm == this);
+
+                 
+            } while (mblStop == false);
+
+
+        }
+
+
+
+
+        //-------------------------------------------------------------------------------------------------
+        // stop timeru
+        //-------------------------------------------------------------------------------------------------
+        public bool Stop()
+        {
+            mobjForm = null;
+            //zastavit thread
+            mblStop = true;
+            return true;
+        }
+    }
+}
+
+  
+    
+    
+   
